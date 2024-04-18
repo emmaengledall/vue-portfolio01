556a4be23edcbae003ad0f88b265c6c6ef662a60
Emma Engledal <55669@dmjx.dk>
2024-04-18 15:06:48 +0200

    last change - better på good

--- portfolio24/src/views/AboutView.vue
+++ portfolio24/src/views/AboutView.vue
@@ -12,35 +12,39 @@ import imagePortrait from '@/assets/pngFiles/portrait01.png';
     <img :src="imagePathAbout" alt="woopss">
   </div>
 
-  
-   <!-- <img :src="imagePortrait" alt="ejjj" class="size-80 opacity-50">-->
-  
-  
 
-  <div class="about font-AndaleMo pl-10 pt-20 flex flex-col text-sm z-10 row-start-1 col-start-1">
+
+  <div class="about absolute font-AndaleMo pl-10 pt-20 text-sm z-20 ">
     <RouterLink to="/">// ABOUT</RouterLink> <!-- WORKS AS A ROUTERLINK TO HOMEPAGE-->
   </div>
    
 
 
 
-<div class="font-AndaleMo pl-10 pt-20 flex flex-col justify-center z-10 row-start-2 col-span-2"> <!--MAIN TEXT ON ABOUT PAGE-->
+<div class="font-AndaleMo pl-10 pt-44 justify-center z-10 row-start-1 col-start-1 "> <!--MAIN TEXT ON ABOUT PAGE-->
   <h1 class="text-3xl">HI THERE, I'M EMMA-CHANTAL. </h1> 
+  <br>
+  <p class="relative text-sm leading-loose">
+  A versatile Frontend Developer, Website Designer, and Coded Designer, driven by a lifelong passion 
+  for art and creativity.  <br> <br>
+  Drawing, painting, building, and storytelling are not just hobbies but natural expressions of my creative spirit. <br>
   
-  <p class="text-sm flex flex-col leading-loose">
-A versatile Frontend Developer, Website Designer, and Coded Designer, driven by a lifelong passion <br>
-for art and creativity. <br> <br>
-Drawing, painting, building, and storytelling are not just hobbies but natural expressions of my creative spirit. <br>
+  In a rapidly evolving technical landscape, being a creative individual opens up boundless opportunities. 
+  From 3D-modeling to coding and designing, I thrive on turning ideas into reality.
+  My top priority is crafting unique, functional websites and coded solutions tailored 
+  to my clients' needs and brand identity. <br> <br>
+  I firmly believe that user experience is paramount, and I'm committed to delivering solutions that are both visually
+  stunning and intuitively functional. <br>
+  Currently, I am pursuing a degree in visual communication at the Danish School of Media and Journalism, specializing in coding. <br>
+  Expected to graduate in June 2026, I am eager to continue pushing the boundaries of creativity and technology.</p>
 
-In a rapidly evolving technical landscape, being a creative individual opens up boundless opportunities. <br>
-From 3D-modeling to coding and designing, I thrive on turning ideas into reality. <br>
-My top priority is crafting unique, functional websites and coded solutions tailored <br>
-to my clients' needs and brand identity. <br> <br>
-I firmly believe that user experience is paramount, and I'm committed to delivering solutions that are both visually stunning and intuitively functional. <br>
-Currently, I am pursuing a degree in visual communication at the Danish School of Media and Journalism, specializing in coding. <br>
- Expected to graduate in June 2026, I am eager to continue pushing the boundaries of creativity and technology</p>
-</div>
+  
+</div>  
 
+
+
+
+<img :src="imagePortrait" alt="ejjj" class="relative w-[30vw] opacity-100 col-start-2 row-start-1 justify-self-center z-20 pt-48 ">
 
 
 </div>
